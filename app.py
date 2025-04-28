3cc82031c7b1e87b7d99dcf6cf4b59e35c396a48
Ali Nawaz <ali.nawaz@doaz.ai>
2025-04-28 16:58:00 +0500

    Update app.py

--- app.py
+++ app.py
@@ -633,7 +633,12 @@ def reformulate_query(original_q: str) -> str:
         rewritten_query = resp.text.strip()
 
         # Log the reformulated query for debugging
-        log_message(f"Reformulated query: {str(rewritten_query)}")
+        # log_message(f"Reformulated query: {str(rewritten_query)}")
+        if rewritten_query.startswith("```"):
+            rewritten_query = rewritten_query.replace("```", "").strip()
+            if rewritten_query.lower().startswith("json"):
+                rewritten_query = rewritten_query[4:].strip()
+                log_message(f"Rewritten query: {rewritten_query}")
 
         # Return the rewritten query, fallback to original if not found
         return rewritten_query if rewritten_query else original_q
@@ -688,7 +693,7 @@ def build_answer_prompt(user_q: str, docs):
 def answer_with_rag(user_q: str):
     log_message("Searching...")
     retrieval_q = reformulate_query(user_q)
-    log_message(f"Retrieval query: {str(retrieval_q)}")
+    log_message(f"Retrieval query: {retrieval_q}")
     docs = retrieve_docs(retrieval_q)
 
     if not docs:
